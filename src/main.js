a7f263ecad3a497368ec6e5c8912bba123bb5374
William Chong <williamchong007@msn.com>
2017-10-27 17:39:22 +0800

    + Add Intercom

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import VueIntercom from 'vue-intercom';
 import 'vue-material/dist/vue-material.css';
 
 import App from './App';
@@ -9,10 +10,19 @@ import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+
+const intercomAppId = process.env.INTERCOM_APP_ID;
+if (intercomAppId) Vue.use(VueIntercom, { appId: intercomAppId });
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App },
+  watch: {
+    '$intercom.ready': function ready() {
+      this.$intercom.boot({ });
+      this.$intercom.show();
+    },
+  },
 });
