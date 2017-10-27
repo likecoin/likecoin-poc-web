b0576c40eab4b37f45502e0a9ae41d364eaf9973
Michael Cheung <michael@lakoo.com>
2017-10-27 15:14:48 +0800

    ! Fix review

--- src/components/ViewList.vue
+++ src/components/ViewList.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="view">
     <md-layout md-gutter>
-      <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="25" v-for="(ipfs, id) in vList" :key="id" :md-item="ipfs">
-        <div class="image-view">
-          <router-link :to="{ name: 'ViewImage', params: { uid: ipfs.id }}">
-            <md-image :md-src="imgUrl(ipfs.ipfs)" />
-          </router-link>
-        </div>
+      <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="25" class="image-view" v-for="(ipfs, id) in vList" :key="id" :md-item="ipfs">
+        <router-link :to="{ name: 'ViewImage', params: { uid: ipfs.id }}">
+          <md-image :md-src="imgUrl(ipfs.ipfs)" />
+        </router-link>
       </md-layout>
     </md-layout>
   </div>
@@ -19,14 +17,12 @@ import { LIKE_MEDIA_ABI, LIKE_MEDIA_ADDRESS, RINKEBY_ID } from '@/constant/contr
 const abi = require('web3-eth-abi');
 
 let eventObj = {};
-for (let i = 0; i < LIKE_MEDIA_ABI.length; i += 1) {
-  if (LIKE_MEDIA_ABI[i].type === 'event' && LIKE_MEDIA_ABI[i].name === 'Uploaded') {
-    eventObj = LIKE_MEDIA_ABI[i];
-    break;
+LIKE_MEDIA_ABI.forEach((obj) => {
+  if (obj.type === 'event' && obj.name === 'Uploaded') {
+    eventObj = obj;
   }
-}
+});
 const signature = abi.encodeEventSignature(eventObj);
-const params = `[{"fromBlock":"0x1","toBlock":"latest","address":"${LIKE_MEDIA_ADDRESS}","topics":["${signature}"]}]`;
 
 export default {
   name: 'ViewList',
@@ -37,15 +33,26 @@ export default {
   },
   methods: {
     refreshList() {
-      const data = `{"jsonrpc":"2.0","method":"eth_getLogs","params":${params},"id":${Math.floor(Math.random() * 100000) + 10}}`;
-      api.apiPostRinkeby(RINKEBY_ID, JSON.parse(data)).then((response) => {
+      const data = {
+        jsonrpc: '2.0',
+        method: 'eth_getLogs',
+        params: [{
+          fromBlock: '0x1',
+          toBlock: 'latest',
+          address: LIKE_MEDIA_ADDRESS,
+          topics: [`${signature}`],
+        }],
+        id: Math.floor(Math.random() * 100000) + 10,
+      };
+      api.apiPostRinkeby(RINKEBY_ID, data).then((response) => {
         // get body data
         const decodeList = response.data.result.map((r) => {
           const decode = abi.decodeLog(eventObj.inputs, r.data, r.topics);
           return { id: r.topics[1], ipfs: decode.ipfs };
         });
         this.vList = decodeList.slice(decodeList.length - 10, decodeList.length);
-      }, (response) => {
+      })
+      .catch((response) => {
         // error callback
         console.log(response);
       });
