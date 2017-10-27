cc862e7f36a8a627a13e658cb58244091b24d31e
William Chong <williamchong007@msn.com>
2017-10-27 22:18:13 +0800

    + Add basic html meta header

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,16 @@
 <script>
 export default {
   name: 'app',
+  metaInfo: {
+    title: 'Meme Generator - showcasing Content Footprint',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: 'favicon.png' },
+    ],
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Meta from 'vue-meta';
 import HelloWorld from '@/components/HelloWorld';
 import UploadImage from '@/components/UploadImage';
 import ViewImage from '@/components/ViewImage';
 import ViewList from '@/components/ViewList';
 
 Vue.use(Router);
+Vue.use(Meta);
 
 export default new Router({
   routes: [
