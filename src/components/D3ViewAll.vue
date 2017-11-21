eac274e45f41f13cb3ba5b693ef3e4aaad47811f
William Chong <williamchong007@msn.com>
2017-11-21 19:49:58 +0800

    = Convert d3 radial tree diagram to d3 force graph

--- src/components/D3ViewAll.vue
+++ src/components/D3ViewAll.vue
@@ -1,6 +1,8 @@
 <template>
-  <svg :width="windowWidth" :height="renderRadius*2" :style="`height:${renderRadius*2}`">
-    <g ref="graph"><g :style="`transform: translate(${Math.max(windowWidth/2, renderRadius)}px, ${renderRadius}px)`">
+  <svg :width="windowWidth" :height="renderRadius*2"
+       @mousemove="drag($event)"
+       @mouseup="drop()">
+    <g>
       <clipPath id="cicleClipPath">
         <circle :r="`${nodeRadius}`"/>
       </clipPath>
@@ -11,30 +13,28 @@
         <stop offset="77%"   stop-color="#b792ff"/>
         <stop offset="100%" stop-color="#fe7ba0"/>
       </radialGradient>
-      <transition-group tag="g" name="line" >
-        <path v-for="link in links" class="link" v-bind:key="link.id" v-bind:d="link.d" ></path>
-      </transition-group>
-      <transition-group tag="g" name="list">
-        <g class="node" v-for="node in renderNodes" v-bind:key="node.id" v-bind:style="node.style">
-          <a :href="'/#/view/'+node.url" :xlink:href="'/#/view/'+node.url">
+        <line v-for="link in graph.links" class="link"
+          :x1="coords[link.source.index].x"
+          :y1="coords[link.source.index].y"
+          :x2="coords[link.target.index].x"
+          :y2="coords[link.target.index].y"></line>
+        <g class="node" v-for="(node, i) in renderNodes" v-bind:style="node.style" >
+          <!-- <a :href="'/#/view/'+node.url" :xlink:href="'/#/view/'+node.url"> -->
           <title>{{ node.description }}</title>
           <image v-if="node.ipfs" :href="'https://meme.like.community/ipfs/'+node.ipfs" clip-path="url(#cicleClipPath)"
            :xlink:href="'https://meme.like.community/ipfs/'+node.ipfs"
+            @mousedown="currentMove = {x: $event.screenX, y: $event.screenY, node: node.dNode}"
            :height="`${nodeSize}`" :width="`${nodeSize}`" :x="`${-nodeRadius}`" :y="`${-nodeRadius}`" /></a>
         </g>
-      </transition-group>
-    </g></g>
+    </g>
   </svg>
 </template>
 
 <script>
+import Vue from 'vue';
 import * as d3 from 'd3';
-import * as panzoom from 'panzoom';
+// import * as panzoom from 'panzoom';
 import * as _debounce from 'lodash.debounce';
-
-function radialPoint(x, y) {
-  return [y * Math.cos(x - (Math.PI / 2)), y * Math.sin(x - (Math.PI / 2))];
-}
 
 export default {
   name: 'D3ViewAll',
@@ -45,10 +45,16 @@ export default {
     return {
       root: null,
       tree: null,
+      graph: {
+        nodes: null,
+        links: null,
+      },
       panzoom: null,
       windowWidth: 0,
       windowHeight: 0,
-      nodeScale: 1,
+      nodeScale: 0.25,
+      padding: 50,
+      currentMove: null,
     };
   },
   computed: {
@@ -56,7 +62,7 @@ export default {
       return this.windowWidth * 0.4;
     },
     nodeBase() {
-      return this.renderRadius * this.nodeScale;
+      return Math.max(this.windowWidth, this.windowHeight) * this.nodeScale;
     },
     nodeSize() {
       return this.nodeBase / 7;
@@ -64,15 +70,10 @@ export default {
     nodeRadius() {
       return this.nodeBase / 14;
     },
-    nodes() {
-      if (this.tree) return this.tree.descendants();
-      return [];
-    },
     renderNodes() {
-      if (this.nodes) {
-        return this.nodes.map((d) => {
-          const [x, y] = radialPoint(d.x, d.y);
-          const transform = `translate(${x}px, ${y}px)`;
+      if (this.graph && this.graph.nodes) {
+        return this.graph.nodes.map((d) => {
+          const transform = `translate(${this.coords[d.index].x}px, ${this.coords[d.index].y}px)`;
           return {
             id: d.id,
             ipfs: d.data.ipfs,
@@ -81,24 +82,30 @@ export default {
             style: {
               transform,
             },
+            dNode: d,
           };
         });
       }
       return [];
     },
-    links() {
-      if (this.root) {
-        return this.root.links().map((d) => {
-          const l = d3.linkRadial()
-                     .angle(i => i.x)
-                     .radius(i => i.y)(d);
-          return {
-            id: d.source.id + d.target.id,
-            d: l,
-          };
-        });
-      }
-      return [];
+    bounds() {
+      return {
+        minX: Math.min(...this.graph.nodes.map(n => n.x)),
+        maxX: Math.max(...this.graph.nodes.map(n => n.x)),
+        minY: Math.min(...this.graph.nodes.map(n => n.y)),
+        maxY: Math.max(...this.graph.nodes.map(n => n.y)),
+      };
+    },
+    coords() {
+      return this.graph.nodes.map((node) => {
+        const padding = 2 * this.padding;
+        return {
+          x: this.padding + (((node.x - this.bounds.minX)
+            * (this.windowWidth - padding)) / (this.bounds.maxX - this.bounds.minX)),
+          y: this.padding + (((node.y - this.bounds.minY)
+            * (this.windowHeight - padding)) / (this.bounds.maxY - this.bounds.minY)),
+        };
+      });
     },
   },
   methods: {
@@ -122,6 +129,39 @@ export default {
                    .separation((a, b) => (a.parent === b.parent ? 2 : 3) / a.depth)(root);
       this.root = root;
       this.tree = tree;
+      Vue.set(this.graph, 'nodes', this.tree.descendants());
+      Vue.set(this.graph, 'links', this.root.links());
+      if (!this.simulation) {
+        this.simulation = d3.forceSimulation(this.graph.nodes)
+          .force('charge', d3.forceManyBody())
+          .force('link', d3.forceLink(this.graph.links).distance(20).strength(1))
+          .force('x', d3.forceX())
+          .force('y', d3.forceY());
+      }
+    },
+    drag(e) {
+      if (this.currentMove) {
+        console.log(e);
+        this.currentMove.node.fx = this.currentMove.node.x -
+         (((this.currentMove.x - e.screenX) * (this.bounds.maxX - this.bounds.minX))
+          / (this.windowWidth - (2 * this.padding)));
+        this.currentMove.node.fy = this.currentMove.node.y -
+        (((this.currentMove.y - e.screenY) * (this.bounds.maxY - this.bounds.minY))
+          / (this.windowHeight - (2 * this.padding)));
+        console.log(this.currentMove.node.fx);
+        console.log(this.currentMove.node.fy);
+        this.currentMove.x = e.screenX;
+        this.currentMove.y = e.screenY;
+      }
+    },
+    drop() {
+      if (this.currentMove) {
+        delete this.currentMove.node.fx;
+        delete this.currentMove.node.fy;
+        this.currentMove = null;
+      }
+      this.simulation.alpha(1);
+      this.simulation.restart();
     },
   },
   mounted() {
@@ -134,17 +174,13 @@ export default {
       }, true);
       this.setWindowWidth();
       this.setWindowHeight();
-      this.panzoom = panzoom(this.$refs.graph);
+      // this.panzoom = panzoom(this.$refs.graph);
+      this.rerenderD3Tree();
     });
   },
   beforeDestroy() {
     window.removeEventListener('resize', _debounce(this.setWindowWidth));
     window.removeEventListener('resize', _debounce(this.setWindowHeight));
-  },
-  watch: {
-    renderRadius() {
-      this.rerenderD3Tree();
-    },
   },
 };
 </script>
